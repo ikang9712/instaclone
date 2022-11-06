a3eefde6050de413612c320e55405719b1d03b54
muse-inho <inho.kang@muse.live>
2022-11-06 15:38:02 -0500

    feature: create account & see profile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -6,33 +6,41 @@ export default {
         createAccount: async(_, 
             {firstName,lastName,username,email,password}
             ) => {
-                // 1.check if username or email are already on DB.
-                const existingUser = await client.user.findFirst({
-                    where: {
-                        OR: [
-                            {
-                                username,
-                            },
-                            {
-                                email,
-                            }
-                        ]
+                try {
+                    // 1.check if username or email are already on DB.
+                    const existingUser = await client.user.findFirst({
+                        where: {
+                            OR: [
+                                {
+                                    username,
+                                },
+                                {
+                                    email,
+                                }
+                            ]
+                        }
+                    })
+                    // ERR: user exists.
+                    if (existingUser){
+                        throw new Error("This username/password is already taken.")
                     }
-                })
-                console.log(existingUser)
-                // 2.hash password
-                // hashing salt, pepper & rainbow table
-                const uglyPassword = await bcrypt.hash(password, 10)
-                // 3.save and return the user
-                // waiting property of browser, so automatically async
-                return client.user.create({
-                    data: {
-                        username,
-                        email,
-                        firstName,
-                        lastName,
-                        password: uglyPassword
-                }})
+                    // 2.hash password
+                    // hashing salt, pepper & rainbow table
+                    const uglyPassword = await bcrypt.hash(password, 10)
+                    // 3.save and return the user
+                    // waiting property of browser, so automatically async
+                    return client.user.create({
+                        data: {
+                            username,
+                            email,
+                            firstName,
+                            lastName,
+                            password: uglyPassword
+                    }})   
+                } catch (error) {
+                    console.log(error)
+                    return error
+                }
         }
     }
 }
