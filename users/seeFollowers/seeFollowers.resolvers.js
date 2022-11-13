3f782cc3f0fe799d32640cf4d54cda0e8a0d312a
muse-inho <inho.kang@muse.live>
2022-11-13 12:13:45 -0500

    feature: seefollowers and off-set paigination. Use of select in gql for less data loading

---
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -0,0 +1,37 @@
+import client from "../../client"
+
+export default {
+    Query: {
+        seeFollowers: async(_, {username, page}) => {
+            const check = await client.user.findUnique({
+                where: {username},
+                select: {id: true},
+            })
+            console.log(check)
+            if (!check){
+                return {
+                    ok: false,
+                    error: "User not found."
+                }
+            }
+            const followers = await client.user
+                                    .findUnique({ where: {username}})
+                                    .followers({
+                                        take: 5, 
+                                        skip: (page-1) * 5,
+                                    })
+            const totalFollowers = await client.user
+                                    .count({ where: {
+                                                following: {
+                                                    some: {username}
+                                                }
+                                    }})
+
+            return {
+                ok: true,
+                followers,
+                totalPages: Math.ceil(totalFollowers / 5)
+            }
+        }
+    }
+}
